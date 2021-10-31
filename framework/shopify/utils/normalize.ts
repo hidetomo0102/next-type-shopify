0f0f123cfeca9abbe26ed2cd6072740a350fb3df
hidetomo0102 <hidenorishio@icloud.com>
2021-10-31 21:06:51 +0900

    initial commit

---
+++ framework/shopify/utils/normalize.ts
@@ -0,0 +1,32 @@
+import { Product as ShopifyProduct, ImageEdge } from "../schema";
+
+const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
+  edges.map(({ node: { originalSrc: url, ...rest } }) => ({
+    url: `/images/${url}`,
+    ...rest,
+  }));
+
+export const normalizeProduct = (productNode: ShopifyProduct): any => {
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: ImageConnection,
+    ...rest
+  } = productNode;
+
+  const product = {
+    id,
+    name,
+    vendor,
+    description,
+    path: `/${handle}`,
+    slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeProductImages(ImageConnection),
+    ...rest,
+  };
+
+  return product;
+};
