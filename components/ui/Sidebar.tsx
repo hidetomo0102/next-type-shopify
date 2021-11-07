6a449d91cfe9a146c73d51789382cb3b85fb32ea
hidetomo0102 <hidenorishio@icloud.com>
2021-11-07 20:28:07 +0900

    optimize provider&sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,4 +1,10 @@
 import { FC, useEffect, useRef } from "react";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
+
 type Props = {
   children: any;
   isOpen: boolean;
@@ -10,8 +16,16 @@ export const Sidebar: FC<Props> = ({ children, isOpen, onClose }) => {
 
   useEffect(() => {
     if (ref.current) {
-      console.log(ref.current);
+      if (isOpen) {
+        disableBodyScroll(ref.current);
+      } else {
+        enableBodyScroll(ref.current);
+      }
     }
+
+    return () => {
+      clearAllBodyScrollLocks();
+    };
   }, [isOpen]);
 
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,13 @@ import { FC } from "react";
 import { AppProps } from "next/app";
 
 import "../assets/main.css";
-import { UIProvider, useUI } from "../components/ui/context";
+import { UIProvider } from "../components/ui/context";
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps & { Component: { Layout: FC } }) {
   const Layout = Component.Layout;
-  const ui = useUI();
-  console.log(ui);
 
   return (
     <UIProvider>
