8cc76e9c7dc9f5851a68c6937edc636707b99861
hidetomo0102 <hidenorishio@icloud.com>
2021-11-10 18:36:59 +0900

    normalize options&variants

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,5 +1,12 @@
 import { Product } from "../../common/types/products";
-import { Product as ShopifyProduct, ImageEdge, MoneyV2 } from "../schema";
+import {
+  Product as ShopifyProduct,
+  ImageEdge,
+  MoneyV2,
+  ProductOption,
+  ProductVariantConnection,
+  SelectedOption,
+} from "../schema";
 
 const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
   edges.map(({ node: { originalSrc: url, ...rest } }) => ({
@@ -12,6 +19,57 @@ const normalizeProductPrice = ({ currencyCode, amount }: MoneyV2) => ({
   currencyCode,
 });
 
+const normalizeProductOption = ({
+  id,
+  values,
+  name: displayName,
+}: ProductOption) => {
+  const normalized = {
+    id,
+    displayName,
+    values: values.map((value) => {
+      let output: any = {
+        label: value,
+      };
+
+      if (displayName.match(/colou?r/gi)) {
+        output = {
+          ...output,
+          hexColor: value,
+        };
+      }
+
+      return output;
+    }),
+  };
+
+  return normalized;
+};
+
+const normalizeProductVariants = ({ edges }: ProductVariantConnection) => {
+  return edges.map(({ node }) => {
+    const { id, selectedOptions, sku, title, priceV2, compareAtPriceV2 } = node;
+
+    return {
+      id,
+      name: title,
+      sku: sku || id,
+      price: +priceV2.amount,
+      listPrice: +compareAtPriceV2?.amount,
+      requireShipping: true,
+      options: selectedOptions.map(({ name, value }: SelectedOption) => {
+        const option = normalizeProductOption({
+          id,
+          name,
+          values: [value],
+        });
+
+        return option;
+      }),
+    };
+  });
+};
+
 export const normalizeProduct = (productNode: ShopifyProduct): Product => {
   const {
     id,
@@ -21,6 +79,8 @@ export const normalizeProduct = (productNode: ShopifyProduct): Product => {
     description,
     images: ImageConnection,
     priceRange,
+    options,
+    variants,
     ...rest
   } = productNode;
 
@@ -33,6 +93,12 @@ export const normalizeProduct = (productNode: ShopifyProduct): Product => {
     slug: handle.replace(/^\/+|\/+$/g, ""),
     images: normalizeProductImages(ImageConnection),
     price: normalizeProductPrice(priceRange.minVariantPrice),
+    options: options
+      ? options
+          .filter((o) => o.name !== "Title")
+          .map((o) => normalizeProductOption(o))
+      : [],
+    variants: variants ? normalizeProductVariants(variants) : [],
     ...rest,
   };
 
